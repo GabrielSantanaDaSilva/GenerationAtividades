5fde19ae7c9917b8f7488d010ef081967002da18
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-06-04 17:19:52 -0300

    ATIVIDADE 1

---
+++ Atividade1.java
@@ -0,0 +1,36 @@
+package generationatividades;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Scanner leia = new Scanner(System.in);
+		
+float salario, abono;
+
+
+
+	System.out.println("Digite seu sálario: ");
+	
+	salario = leia.nextFloat();
+	
+	System.out.println("Digite o seu abono: ");
+	
+	abono = leia.nextFloat();
+	
+	System.out.println("Segue abaixo a atualização de seu salario: ");
+	
+	System.out.println((salario + abono));
+	
+	
+			
+			
+			
+		
+		
+	}
+
+}
