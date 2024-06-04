8c88ce203e8221f2b0893abe8d73a5c496896395
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-06-04 17:35:27 -0300

    Segunda atividade

--- Atividade1.java
+++ Atividade1.java
@@ -7,7 +7,7 @@ public class Atividade1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	Scanner leia = new Scanner(System.in);
+	Scanner leia = new Scanner(System.in); 
 		
 float salario, abono;
 
---
+++ Atividade2.java
@@ -0,0 +1,38 @@
+package generationatividades;
+
+import java.util.Scanner;
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		// TODO Auto-generated method stub
+
+		float nota1, nota2, nota3, nota4, media;
+		
+		System.out.println("Qual foi a sua primeira nota? ");
+		 
+		nota1 = leia.nextFloat();
+		
+		System.out.println("Qual foi sua segunda nota? ");
+		
+		nota2 = leia.nextFloat();
+		
+		System.out.println("Qual foi a sua terceira nota? ");
+		
+		nota3 = leia.nextFloat();
+		
+		System.out.println("Qual foi sua quarta nota? ");
+		
+		nota4 = leia.nextFloat();
+		
+		System.out.println("Segue abaixo sua média final: ");
+		System.out.println((nota1+nota2+nota3+nota4)/4);
+		
+		
+
+	}
+
+}
