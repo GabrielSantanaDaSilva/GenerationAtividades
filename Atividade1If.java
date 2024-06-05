48343a14a901d2d01c5d27c32b2bb1b404adbdaa
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-06-05 16:09:10 -0300

    Atividades IF ELSE

---
+++ Atividade1If.java
@@ -0,0 +1,42 @@
+package generationatividades;
+
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Atividade1If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+
+		int valor1, valor2, valor3;
+
+		System.out.println("Digite um número: ");
+		valor1 = leia.nextInt();
+
+		System.out.println("Digite um número: ");
+		valor2 = leia.nextInt();
+
+		System.out.println("Digite um número: ");
+		valor3 = leia.nextInt();
+
+		if (valor1 + valor2 > valor3) {
+
+			System.out.println("A soma de " + valor1 + " + " + valor2 + " é maior que " + valor3 + ".");
+
+		}
+		if (valor1 + valor2 < valor3) {
+
+			System.out.println("A soma de " + valor1 + " + " + valor2 + " é menor que " + valor3 + ".");
+
+		}
+		if (valor1 + valor2 == valor3) {
+
+			System.out.println("A soma de " + valor1 + " + " + valor2 + " é igual a " + valor3 + ".");
+
+		}
+
+	}
+
+}
---
+++ Atividade2If.java
@@ -0,0 +1,42 @@
+package generationatividades;
+import java.util.Scanner;
+
+public class Atividade2If {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		// TODO Auto-generated method stub
+		
+		int numero ;
+		
+		System.out.println("Digite um número");
+		numero = leia.nextInt();
+		
+			  if(numero % 2 == 0 && numero > 0) {
+			
+			System.out.println("O número "+numero+" é par e positivo");
+			
+		}else if(numero % 2 == 0 && numero < 0) {
+			
+			System.out.println("O número "+numero+" é par e negativo");
+			
+		}else if(numero % 2 != 0 && numero > 0) {
+			
+			System.out.println("O número "+numero+" é impar e positivo");
+			
+		}else if(numero % 2 != 0 && numero < 0) {
+			
+			System.out.println("O número "+numero+" é impar e negativo");
+			
+		}else{
+			
+			System.out.println("O número é igual a 0");
+			
+		}
+		
+
+	}
+
+}
