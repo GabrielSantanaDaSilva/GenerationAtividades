3e5e40fb00af5f0c8d889f3dc6de3d6848475562
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-06-06 13:21:36 -0300

    Atividade 1 FOR

---
+++ Atividade1For.java
@@ -0,0 +1,38 @@
+package generationatividades;
+
+import java.util.Scanner;
+
+public class Atividade1For {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		int numero1, numero2;
+
+		System.out.println("Digite um número: ");
+		numero1 = leia.nextInt();
+
+		System.out.println("Digite um numero maior que o primeiro: ");
+		numero2 = leia.nextInt();
+
+		if (numero1 > numero2) {
+			
+			System.out.println("Intervalo Inválido");
+			
+		} else {
+			System.out.println("No intervalo entre " + numero1 + " e " + numero2 + ": ");
+			
+			for (int i = numero1; i < numero2; i++) {
+
+				if (i % 3 == 0 && i % 5 == 0) {
+
+					System.out.println("O número " + i + " é multiplo de 3 e 5.");
+
+				}
+
+			}
+
+		}
+	}
+}
